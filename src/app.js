ad0817f6918e2221e31485d32c0c332d85a6eb59
Marcelo de Borba <marcelo.deborba@hotmail.com>
2020-08-23 18:56:07 -0300

    Desafio conceitos de NodeJS

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,76 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+
+  const findRepIndex = repositories.findIndex(repository => repository.id === id );
+
+  if(findRepIndex === -1){
+    return response.status(400).json({error: 'O repositório não existe.'})
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[findRepIndex].likes,
+  };
+
+  repositories[findRepIndex] = repository;
+
+  return response.json(repository);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const findRepIndex = repositories.findIndex(repository => repository.id === id );
+
+  if (findRepIndex > -1) {
+    repositories.splice(findRepIndex, 1);
+  } else {
+    response.status(400).send(json({error: 'Repositório não existe.'}));
+  }
+  
+  return response.status(204).send();  
+  
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const findRepIndex = repositories.findIndex(repository => repository.id === id );
+
+  if (findRepIndex === -1){
+    return response.status(400).json({error: 'O repositório não existe.'});
+  }
+
+  repositories[findRepIndex].likes++;
+
+  return response.json(repositories[findRepIndex]);
 });
 
 module.exports = app;
